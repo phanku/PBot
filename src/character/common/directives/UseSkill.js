aade65b9e71362c4c837230604c2cc524a4cc350
Joseph Pahl <phanku@vt.edu>
2020-02-26 12:26:24 -0500

    Added Stubbed UseSkill directive.

---
+++ src/character/common/directives/UseSkill.js
@@ -0,0 +1,19 @@
+/**
+ * Provides the directive to cause the character to use a skill.
+ * @author Joseph Pahl <https://github.com/phanku/>
+ * @version 0.26.0_017_75f2a70_2020-02-26_12:26:24
+ * @since 0.26.0_017_75f2a70_2020-02-26_12:26:24
+ */
+
+// Imports.
+const C = require('../../../game/Constants');
+const Logger = require('../../../game/logger/Logger').default;
+
+/**
+ * Provides the skill usage behavior.
+ * @param skill The skill.
+ * @param bot The character.
+ */
+module.exports = (skill, bot) => {
+
+};
