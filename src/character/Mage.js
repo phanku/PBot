68a74ce96d2dd962290787db19c89b2047f9d1bd
Joseph Pahl <phanku@vt.edu>
2020-03-10 19:30:14 -0400

    - Commented out Use Mana Burst directive.

--- src/character/Mage.js
+++ src/character/Mage.js
@@ -2,7 +2,7 @@
  * Provides the mage character class.
  * Note: Very alpha character.
  * @author Joseph Pahl <https://github.com/phanku/>
- * @version 0.27.0_032_c75b903_2020-03-10_16:26:52
+ * @version 0.29.0_034_8665ddf_2020-03-10_19:30:14
  * @since 0.22.0_001_d08612d_2020-02-24_08:53:57
  */
 
@@ -25,7 +25,7 @@ export class Mage extends Character {
 
         // Setting the default behavior for the character class.
         this.directives.enqueue(require('./common/directives/RangeAttack').default(this));
-        this.directives.enqueue(require('./common/directives/UseManaBurst').default(this));
+        // this.directives.enqueue(require('./common/directives/UseManaBurst').default(this));
 
         // Logging debug message.
         Logger.log(Logger.DEBUG, "Mage character loaded.");
